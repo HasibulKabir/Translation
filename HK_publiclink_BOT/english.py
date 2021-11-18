a4ff1a9ea112bffa4ee354b30d29b8b58783e496
Fayas Noushad <TheFayasNoushad@gmail.com>
2021-11-18 21:17:18 +0530

    Update and Rename

--- HK_publiclink_BOT/english.py
+++
@@ -1,6 +0,0 @@
-class English:
-    START = "START COMMAND TEXT"
-    DW_Progress = "Downloading file in progress. If you want to cancel current progress hit cancel button."
-    Upload_Progress = "Uploading to external host.. please wait few time.."
-    Uploaded = "Link generated. Filename: {} Size: {} Link: {}"
-    time_wait = "I can't handle too much file at a time. Please send one file at a time. You can send a file every 10 seconds. Time left: {} seconds."
